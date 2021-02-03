e96955131c2957dab43258c3a4247fdaf159f82b
chica25 <patricia@patriciaco.com>
2021-02-03 10:47:52 -0500

    Set directories and files

---
+++ src/components/Language.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Language extends Component {
+
+    render() {
+        return(
+            <div className="language">languages</div>
+           
+        )
+    }
+    
+}
+
+export default Language;
