358bf808a69019b64682042c58aa819b0d04b80d
chica25 <patricia@patriciaco.com>
2021-02-07 11:35:16 -0500

    Set additional routes and Interactive Map component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import LanguageList from '/components/LanguageList';
+import InteractiveMaps from '/components/InteractiveMaps';
 
 class App extends React.Component {
   render(){
@@ -8,6 +10,9 @@ class App extends React.Component {
        <Router>
         <Switch>
           <Route exact path="/" component={Home}/>
+          <Router path="/languages" component={LanguageList}/>
+          <Router path="/phrases" component={Phrases}/>
+          <Router path="/interactivemaps" component={InteractiveMaps}/>
         </Switch>
        </Router>
       );
---
+++ src/components/InteractiveMaps.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class InteractiveMaps extends React.Components {
+
+}
+
+export default InteractiveMaps
