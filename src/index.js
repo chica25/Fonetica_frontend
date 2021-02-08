4a8b8a9beea5f5d5dc321b56efbdb16853f394be
chica25 <patricia@patriciaco.com>
2021-02-08 17:42:26 -0500

    Import PracticePhraseReduceto index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger'; 
 import { LanguageReducer } from '../reducers/LanguageReducer';
 import { LanguagePhrasesReducer } from '../reducers/LanguagePhrasesReducer';
-
+import { PracticePhraseReducer } from, '../reducers/PracticePhraseReducer';
 
 const store = createStore(LanguageReducer, LanguagePhrasesReducer, compose(applyMiddleware(thunk, logger),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
--- src/redux/reducers/PracticePhraseReducer.js
+++ src/redux/reducers/PracticePhraseReducer.js
@@ -5,7 +5,7 @@ const initialState = {
 export const PracticePhraseReducer = (state = initialState, action) => {
     switch(action.type){
         case "PRACTICE_PHRASE":
-            return {...state, PracticePhrase: [...state.practicePhrase, action.payload] }
+            return {...state, practicePhrase: [...state.practicePhrase, action.payload] }
         default:
           return state;
     }
