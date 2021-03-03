8c5fb7ffa19eefbf6bf45b70eaedd8903a6535a1
chica25 <patricia@patriciaco.com>
2021-03-03 00:28:20 -0500

    Create InteractiveMaps component

---
+++ src/components/InteractiveMaps.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class InteractiveMaps extends React.Component {
+
+    render() {
+        return(
+            <div>All Maps</div>
+        )
+    }
+}
+
+export default InteractiveMaps;
